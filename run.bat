dd671a49848e9b9d51c0ca4d8272f44817a845d0
Anusha-Mohanty <149676456+Anusha-Mohanty@users.noreply.github.com>
2025-06-17 10:53:31 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -4,5 +4,5 @@ set PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true
 set PUPPETEER_EXECUTABLE_PATH=node_modules\whatsapp-web.js\node_modules\puppeteer-core\.local-chromium\win64-1045629\chrome-win\chrome.exe
 set GOOGLE_SHEET_ID=1SsGMtuOuKw2GexadE9Qx9PT-bwNdsaB92TgtEulOj4w
 set NODE_PATH=node_modules
-whatsapp-scheduler-bot.exe
-pause 
+node start.js
+pause
